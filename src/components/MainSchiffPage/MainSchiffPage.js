a7da7492864c9408a3e4fe967bd967c8ed986a1c
HunterJerochim <hunter.jerochim@gmail.com>
2024-11-14 22:24:44 -0800

    Add Goblin Schiff

--- src/components/MainSchiffPage/MainSchiffPage.js
+++ src/components/MainSchiffPage/MainSchiffPage.js
@@ -12,13 +12,33 @@ const COIN_CAVE_BACKGROUND_IMAGE =
   "https://res.cloudinary.com/dav8yugzm/image/upload/v1731549155/pile-of-gold-schiff-website-2_sag7c0.png";
 const MP3_FILE =
   "https://res.cloudinary.com/dav8yugzm/video/upload/v1731638389/Path_of_Exile_-_Goblin_Troupe_Pet_Drum_Bass_ovld7o.mp3";
+const SCHIFF_GOBLIN =
+  "https://res.cloudinary.com/dav8yugzm/image/upload/v1731651346/schiff-goblin_y6dlsj.gif";
 
 const MainSchiffPage = () => {
   const [showModal, setShowModal] = useState(false);
-  const audioRef = useRef(null); 
+  const audioRef = useRef(null);
+  const [position, setPosition] = useState({ top: "0px", left: "0px" });
 
   useEffect(() => {
     setShowModal(true);
+  }, []);
+
+  useEffect(() => {
+    const moveGoblin = () => {
+      const goblinWidth = 150;
+      const goblinHeight = 150;
+      const maxWidth = window.innerWidth - goblinWidth;
+      const maxHeight = window.innerHeight - goblinHeight;
+      const newLeft = Math.floor(Math.random() * maxWidth);
+      const newTop = Math.floor(Math.random() * maxHeight);
+      setPosition({ top: `${newTop}px`, left: `${newLeft}px` });
+    };
+
+    const intervalId = setInterval(moveGoblin, 3000);
+    moveGoblin();
+
+    return () => clearInterval(intervalId);
   }, []);
 
   const handleCloseModal = () => {
@@ -35,13 +55,17 @@ const MainSchiffPage = () => {
 
   return (
     <MainSchiffPageContainer>
-      <Header /> 
+      <Header />
       <SchiffSocials />
       <ContractAddress />
       <SchiffGraph />
       <GridLayout />
       {showModal && <Modal onClose={handleCloseModal} playAudio={playAudio} />}
-
+      <GoblinImage
+        src={SCHIFF_GOBLIN}
+        style={{ top: position.top, left: position.left }}
+        alt="Schiff Goblin"
+      />
       <audio ref={audioRef} src={MP3_FILE} loop />
     </MainSchiffPageContainer>
   );
@@ -50,6 +74,7 @@ const MainSchiffPage = () => {
 export default MainSchiffPage;
 
 const MainSchiffPageContainer = styled.div`
+  position: relative;
   background-image: url(${COIN_CAVE_BACKGROUND_IMAGE});
   background-size: cover;
   background-attachment: fixed;
@@ -77,4 +102,13 @@ const MainSchiffPageContainer = styled.div`
 
   scrollbar-width: thin;
   scrollbar-color: rgba(0, 0, 0, 0.3) transparent;
+`;
+
+const GoblinImage = styled.img`
+  position: absolute;
+  width: 150px;
+  height: auto;
+  transition: top 2.5s ease, left 2.5s ease;
+  z-index: 1000;
+  pointer-events: none;
 `;
